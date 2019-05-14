15dd4341966b1713c5a7cb752f67ced540e559ce
RafaelaSilveira <rafaelacss21@gmail.com>
2019-05-13 22:03:05 -0400

    add filter

--- js/app.js
+++ js/app.js
@@ -3,3 +3,25 @@ document.getElementById("cart-info").addEventListener("click", function() {
   cart.classList.toggle("show-cart");
   console.log(cart);
 });
+
+const FilterItems = (() => {
+  const filter = document.querySelectorAll(".filterBtn");
+
+  filter.forEach(btn => {
+    btn.addEventListener("click", event => {
+      const value = event.target.dataset.filter;
+      // console.log(value);
+      const storeItem = document.querySelectorAll(".store-item");
+      // console.log(store-item);
+      storeItem.forEach(item => {
+        if (value === "all") {
+          item.style.display = "block";
+        } else {
+          !item.classList.contains(value)
+            ? (item.style.display = "none")
+            : (item.style.display = "block");
+        }
+      });
+    });
+  });
+})();
